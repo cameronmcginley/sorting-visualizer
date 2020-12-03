ac25ce3a09a7ad01c662e1e3ab4a979d68a2215b
Cameron McGinley <mcginleycameron@gmail.com>
2020-12-03 14:51:35 -0600

    Switched to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
-import React, { Component } from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 //In package.json:
 //"homepage": "http://cameronmcginley.com/sorting-visualizer",
 
 function App() {
     return (
-        <HashRouter basename="/">
+        <BrowserRouter basename="/">
             <div className="App">
                 <SortingVisualizer></SortingVisualizer>
             </div>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
